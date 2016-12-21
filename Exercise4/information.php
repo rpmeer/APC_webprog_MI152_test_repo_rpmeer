f131f5d4e25fa9f3ccdaf55c4fc2e921f25c482a
rpmeer <reinanpmeer@gmail.com>
2016-12-21 16:49:31 +0800

    Changed to get

--- Exercise4/information.php
+++ Exercise4/information.php
@@ -13,7 +13,7 @@
   
   <a href="HomePage.html" style="position: absolute; top: 120px; left: 50px;"><img src="house.png" style="height: 40px; width: 40px; position: absolute; top: -40px; left: 15px;">Home</a>
   <a href="About.html" style="position: absolute; top: 250px; left: 50px;"><img src="me.png" style="height: 40px; width: 40px; position: absolute; top: -40px; left: 15px;">About</a>
-  <a href="information.html" style="position: absolute; top: 380px; left: 25px;"><img src="info.png" style="height: 40px; width: 40px; position: absolute; top: -40px; left: 40px;">Information</a>
+  <a href="information.php" style="position: absolute; top: 380px; left: 25px;"><img src="info.png" style="height: 40px; width: 40px; position: absolute; top: -40px; left: 40px;">Information</a>
 </div>
 
 <!--first layer-->
@@ -27,47 +27,49 @@
     $nameErr = $emailErr = $genderErr = $nicknameErr = $cellnoErr = "";
     $name = $email = $gender = $comment = $address = $cellno = $nickname = "";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      if (empty($_POST["name"])) {
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+      if (empty($_GET["name"])) {
         $nameErr = "Name is required";
       } else {
-        $name = test_input($_POST["name"]);
+        $name = test_input($_GET["name"]);
       }
       
-      if (empty($_POST["email"])) {
+      if (empty($_GET["email"])) {
         $emailErr = "Email is required";
       } else {
-        $email = test_input($_POST["email"]);
+        $email = test_input($_GET["email"]);
       }
 
-      if (empty($_POST["nickname"])) {
+      if (empty($_GET["cellno"])) {
+        $cellnoErr = "Cellphone Number is required";
+      } else {
+        $cellno = test_input($_GET["cellno"]);
+      }
+      
+
+      if (empty($_GET["nickname"])) {
         $nicknameErr = "Nickname is required";
       } else {
-        $nickname = test_input($_POST["nickname"]);
+        $nickname = test_input($_GET["nickname"]);
       }
 
-      if (empty($_POST["cellno"])) {
-        $cellno = "Cellphone Number is required";
-      } else {
-        $cellno = test_input($_POST["cellno"]);
-      }
-        
-      if (empty($_POST["address"])) {
+              
+      if (empty($_GET["address"])) {
         $address = "";
       } else {
-        $address = test_input($_POST["address"]);
+        $address = test_input($_GET["address"]);
       }
 
-      if (empty($_POST["comment"])) {
+      if (empty($_GET["comment"])) {
         $comment = "";
       } else {
-        $comment = test_input($_POST["comment"]);
+        $comment = test_input($_GET["comment"]);
       }
 
-      if (empty($_POST["gender"])) {
+      if (empty($_GET["gender"])) {
         $genderErr = "Gender is required";
       } else {
-        $gender = test_input($_POST["gender"]);
+        $gender = test_input($_GET["gender"]);
       }
     }
 
@@ -80,12 +82,12 @@
     ?>
 
 
-    <form method="post" style="position: absolute; top: 10px; left: 300px;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form method="GET" style="position: absolute; top: 10px; left: 300px;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
       <p style="font-size: 30px;"><b>Personal Information</b></p>
-
+      <p style="color: red;">* required field</p>
       Name: <input type="text" name="name">
-      <span class="error">* <?php echo $nameErr;?></span>
+      <span class="error" style="color: red;">* <?php echo $nameErr;?></span>
       <br><br>
       Nickname: <input type="text" name="nickname">
       <span class="error">* <?php echo $nicknameErr;?></span>
@@ -102,7 +104,7 @@
       <input type="radio" name="gender" value="male">Male
       <span class="error">* <?php echo $genderErr;?></span>
       <br><br>
-      Cellphone Number: <input type="text" name="nickname">
+      Cellphone Number: <input type="text" name="cellno">
       <span class="error">* <?php echo $cellnoErr;?></span>
       <br><br>
       Comment: <textarea name="comment" rows="5" cols="40"></textarea>
